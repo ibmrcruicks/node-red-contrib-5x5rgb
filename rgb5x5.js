d6c2995452a91ecea5cf17fd9f75fc45c3d78ff1
Ross Cruickshank <ross.cruickshank@uk.ibm.com>
2021-04-09 17:20:28 +0100

    Create rgb5x5.js

---
+++ rgb5x5.js
@@ -0,0 +1,38 @@
+
+module.exports = function(RED) {
+    "use strict"
+
+    function rgb5x5NodeOut(n) {
+        RED.nodes.createNode(this,n);
+        this.buttonState = -1;
+        this.bus = n.bus;
+        this.address = n.address;
+        this.command = n.command;
+        this.matrix = require("rgb5x5matrix.js");
+      
+        var node = this;
+        node.on("input", function(msg) {
+                            if (node.command === "msg") {
+                              node.command = msg.payload;
+                            }
+                            if (node.command === "on") {  
+                              node.matrix.on();
+                            }
+                            if (node.command === "off") {
+                              node.matrix.off();
+                            }
+                            if (node.command === "clear") {
+                              node.matrix.clear();
+                            }
+                            if (node.command === "msg") {
+                              node.warn(`checking contents of ${msg.payload}`);
+                            }
+                        }
+                    });
+        }
+        node.on('close', function() {
+            node.matrix.off();
+        });
+    }
+    RED.nodes.registerType("rgb5x5",rgb5x5NodeOut);
+}
